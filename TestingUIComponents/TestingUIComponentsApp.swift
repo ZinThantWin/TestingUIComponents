d5481a156a10440b6f943390e059d40a8fb100c7
ZinThantWin <zinthantwin1997@gmail.com>
2024-06-21 10:54:17 +0630

    Initial Commit

---
+++ TestingUIComponents/TestingUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingUIComponentsApp.swift
+//  TestingUIComponents
+//
+//  Created by ကင်ဇို on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestingUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
